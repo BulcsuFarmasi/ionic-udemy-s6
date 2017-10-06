7cf82e96903ffee2b0955fb7dcbdf8b5a57d834a
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-06 21:47:03 +0200

    loading and displaying shopping list items

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { ShoppingListService } from '../../services/shopping-list';
+import { Ingredient } from '../../models/ingredient';
 
 @Component({
   selector: 'page-shopping-list',
@@ -9,12 +10,23 @@ import { ShoppingListService } from '../../services/shopping-list';
 })
 export class ShoppingListPage {
     
+    public listItems:Ingredient[] = [];
+    
     constructor (private shoppingListService:ShoppingListService){}
     
+    ionViewWillEnter () {
+        this.loadItems();
+    }
+
     onAddItem (form:NgForm) {
         const name = form.value.ingredientName;
         const amount = form.value.amount;
         this.shoppingListService.addItem(name, amount);
+        this.loadItems();
         form.reset();
+    }
+
+    loadItems () {
+        this.listItems = this.shoppingListService.getItems();
     }
 }
