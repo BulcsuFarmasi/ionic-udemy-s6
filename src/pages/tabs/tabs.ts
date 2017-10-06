988693b437df8b227b62823bcff8ab38c279e74b
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-06 18:23:18 +0200

    adding tabs navigation

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,25 +1,16 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RecipesPage } from '../recipes/recipes';
+import { ShoppingListPage } from '../shopping-list/shopping-list';
 
-/**
- * Generated class for the TabsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
+    public recipesPage = RecipesPage;
+    public shoppingListPage = ShoppingListPage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
-  }
-
+    constructor () {
+        console.log(this.shoppingListPage);
+    }
 }
