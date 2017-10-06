1a28190d1800c30bb0a07deb9dabdb0124db597c
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-06 19:58:48 +0200

    add shopping list service and ingredieent model

---
+++ src/services/shopping-list.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core'
+import { Ingredient } from '../models/ingredient';
+
+@Injectable()
+export class ShoppingListService {
+    private ingredients:Ingredient[] = [];
+
+    addItem (name:string, amount:number) {
+        this.ingredients.push(new Ingredient(name, amount));
+    }
+
+    addItems (items: Ingredient[]) {
+        this.ingredients.push(...items);
+    }
+
+    getItems() {
+        return this.ingredients.slice();
+    }
+
+    removeItem (index:number) {
+        this.ingredients.splice(index, 1)
+    }
+}
