d1b3487654bbd38a12ea013cae9bf9e949dc717f
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-07 11:50:20 +0200

    creating form programatacly

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { NavParams } from 'ionic-angular';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-edit-recipe',
@@ -9,12 +10,21 @@ import { NavParams } from 'ionic-angular';
 export class EditRecipePage implements OnInit {
 
    public mode:string = 'New';
+   public recipeForm:FormGroup;
    public selectOptions:string[] = ['Easy', 'Medium', 'Hard'];
 
   constructor (private navParams:NavParams) {}
 
   ngOnInit () {
       this.mode = this.navParams.get('mode');
+  }
+
+  initializeForm () {
+      this.recipeForm = new FormGroup({
+          title: new FormControl(null, Validators.required),
+          description: new FormControl(null, Validators.required),
+          difficulty: new FormControl('Medium', Validators.required)
+      })
   }
   
 }
