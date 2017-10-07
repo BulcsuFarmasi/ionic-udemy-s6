2cb20578566b212838d820e7c629a5c2ac26bc11
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-07 11:38:35 +0200

    setting up recipe form

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-new-recipe',
+  selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage {
+export class EditRecipePage implements OnInit {
 
+   public mode:string = 'New';
+   public selectOptions:string[] = ['Easy', 'Medium', 'Hard'];
+
+  constructor (private navParams:NavParams) {}
+
+  ngOnInit () {
+      this.mode = this.navParams.get('mode');
+  }
+  
 }
--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
+import { NavController } from 'ionic-angular';
+import { EditRecipePage } from '../edit-recipe/edit-recipe';
+
 @Component({
   selector: 'page-recipes',
   templateUrl: 'recipes.html',
 })
 export class RecipesPage {
 
+  constructor (private navController:NavController) {}
+  
+  onNewRecipe () {
+    this.navController.push(EditRecipePage, {mode: 'New'});
+  }
+  
 }
