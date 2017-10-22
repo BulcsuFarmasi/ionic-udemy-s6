81d1781707e3c32f5f6514a253300a0816a6f85c
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-22 12:23:30 +0200

    initializing firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,12 @@ import { MenuController, NavController, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import firebase from 'firebase';
+
 import { SigninPage } from '../pages/signin/signin';
 import { SignupPage } from '../pages/signup/signup';
 import { TabsPage } from '../pages/tabs/tabs';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -18,6 +21,10 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
      private menuController:MenuController) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyAinpZUmMQlkO-zlZaqutHNxoc_XZmOuvo",
+        authDomain: "ionic-recipe-book-15ab7.firebaseapp.com
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
