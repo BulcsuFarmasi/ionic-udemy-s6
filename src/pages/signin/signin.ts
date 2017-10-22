888b929a4e83508676a1b94fa0b78fdc7f0daadc
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-22 13:29:23 +0200

    showing loader and alert

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AlertController, LoadingController } from 'ionic-angular';
+
 import { AuthService } from '../../services/auth';
 
 @Component({
@@ -9,15 +11,27 @@ import { AuthService } from '../../services/auth';
 })
 export class SigninPage {
 
-  constructor (private authService:AuthService) {}
+  constructor (private authService:AuthService,
+               private loadingController:LoadingController,
+               private alertController:AlertController) {}
 
   onSignin (form:NgForm) {
+      const loading = this.loadingController.create({
+          content: 'Signing you in...'
+      })
+      loading.present();
       this.authService.signin(form.value.email, form.value.password)
           .then(data => {
-              console.log(data)
+              loading.dismiss();
           })
           .catch(error => {
-              console.log(error)
+             loading.dismiss();
+             const alert = this.alertController.create({
+                 title: 'Signin falied!',
+                 message: error.message,
+                 buttons: ['Ok']
+             });
+             alert.present()
           })
   }
 
