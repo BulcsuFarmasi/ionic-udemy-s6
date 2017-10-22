7324de6a28586e5f937b69ca3a4a82bd548d88ee
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-22 13:18:04 +0200

    implementing sign in

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,7 +4,12 @@ import firebase from 'firebase';
 
 @Injectable()
 export class AuthService {
-    signup (email: string, password: string) {
+
+    signup (email:string, password:string) {
         return firebase.auth().createUserWithEmailAndPassword(email, password);
+    }
+
+    signin (email:string, password:string) {
+        return firebase.auth().signInWithEmailAndPassword(email, password);
     }
 }
